aafd61115804e77a8dad0c902405e841a8140615
panjiegary <panjiegary@gmail.com>
2017-03-06 13:52:38 -0400

    123

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 2 ]; then
     echo "Please specify version of Hadoop and Spark" && exit 1
 fi
 
-hadoop_version = $1
-spark_version = $2
-echo "$hadoop_version"
-echo "$spark_version"
+hadoop_version=$1
+spark_version=$2
+echo "Hadoop:$hadoop_version"
+echo "Spark:$spark_version"
