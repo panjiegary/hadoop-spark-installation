957543fb079795fc84f9b442e785642e84d6aa22
panjiegary <panjiegary@gmail.com>
2017-03-06 13:24:07 -0400

    added install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# check input arguments
+if [ "$#" -ne 2 ]; then
+    echo "Please specify version of Hadoop and Spark" && exit 1
+fi
+
+hadoop_version = $1
+spark_version = $2
+echo hadoop_version
+echo spark_version
