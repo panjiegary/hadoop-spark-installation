664926d514a8b8ee0becee7c26ada5111b18c836
panjiegary <panjiegary@gmail.com>
2017-03-10 19:36:37 -0400

    123

--- install.sh
+++ install.sh
@@ -22,13 +22,13 @@ sudo apt-get install openssh-server -y
 
 echo "Installing Hadoop"
 cd /usr/local
-wget http://www-us.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+sudo wget http://www-us.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
 
-tar -xzvf hadoop-2.7.3.tar.gz  
+sudo tar -xzvf hadoop-2.7.3.tar.gz  
 
 sudo mkdir hadoop  
 
-mv hadoop-2.7.3/* hadoop/  
+sudo mv hadoop-2.7.3/* hadoop/  
 
 echo "Now script is updating hadoop configuration files"  
 
@@ -36,11 +36,11 @@ cat >> /usr/local/hadoop/etc/hadoop/hadoop-env.sh << EOL
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")  
 EOL  
 
-wget https://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz
+sudo wget https://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz
 
-tar -zxvf spark-2.1.0-bin-hadoop2.7.tgz
+sudo tar -zxvf spark-2.1.0-bin-hadoop2.7.tgz
 sudo mkdir spark
-mv spark-2.1.0-bin-hadoop2.7/* /usr/local/spark/
+sudo mv spark-2.1.0-bin-hadoop2.7/* /usr/local/spark/
 
 cat >> ~/.bashrc << EOL
 export SPARK_HOME=/usr/local/spark/
